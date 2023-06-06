bcb7c320c2a6235223c4dd97804435aa620e2b98
Andrii <andriy.kovtunets@gmail.com>
2023-06-06 22:18:53 +0200

    change React project to based on functions component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
+const App = () => {
+  const [searchField, setSearchField] = useState(" ");
+  const [monsters, setMonster] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonster(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+/* class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -36,22 +72,12 @@ class App extends Component {
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
 
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
+   
 
     return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          className="monsters-search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
+     
     );
   }
 }
-
+ */
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,13 @@
-import { Component } from "react";
-
 import Card from "../card/card.component.jsx";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    console.log(this.props);
-    const time = new Date().getMilliseconds();
-    console.log(`CardList - ${time}`);
-    const { monsters } = this.props;
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  // const { monsters } = props; - this is restructuring props
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster} />;
+    })}
+  </div>;
+};
 
 export default CardList;
